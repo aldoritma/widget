be88253db4ae012185946db2b6d9ede1765ab0a1
aldoariandhika@gmail.com <aldoariandhika@gmail.com>
2015-02-10 23:04:19 +0700

    add ng-repeat on payment method

--- javascript/app.js
+++ javascript/app.js
@@ -1,4 +1,4 @@
-var app = angular.module('formApp',['ngAnimate', 'ui.router','mm.foundation']);
+var app = angular.module('payment',['ngAnimate', 'ui.router','mm.foundation']);
 
 
 app.config(function($stateProvider, $urlRouterProvider) {
@@ -6,12 +6,18 @@ app.config(function($stateProvider, $urlRouterProvider) {
     $stateProvider
 
         // route to show our basic form (/form)
+
+
         .state('payment', {
             url: '/payment',
             templateUrl: 'views/payment.html',
             controller: 'formController'
         })
 
+        .state('home',{
+          url: '/home',
+          templateUrl: 'views/home.html'
+        })
         // nested states
         // each of these sections will have their own view
         // url will be nested (/form/profile)
@@ -36,10 +42,23 @@ app.config(function($stateProvider, $urlRouterProvider) {
 
     // catch all route
     // send users to the form page
-    $urlRouterProvider.otherwise('/payment/method');
+    $urlRouterProvider.otherwise('/home');
 });
 
 
+// payment method controller
+/*
+var paymentController = function ($scope, $http){
+
+  $http.get('views/data/widget.json')
+      .success(function(data) {
+        $scope.datas = data;
+      })
+      .error(function(data,status,headers,config)
+      { console.log('error render ajax json'); }
+    );
+};
+*/
 
 var formController = function ($scope)
 {
@@ -57,27 +76,8 @@ var formController = function ($scope)
 
 };
 
-//----------------
-//    Accordion
-//----------------
 
-var Accordion = function ($scope)
-{
-  $scope.oneAtATime = true;
 
-   $scope.items = ['Item 1', 'Item 2', 'Item 3'];
-
-   $scope.addItem = function() {
-     var newItemNo = $scope.items.length + 1;
-     $scope.items.push('Item ' + newItemNo);
-   };
-};
-
-app.directive('Step',function($parse){
-  return{
-
-  }
-});
 
 
 var Dropdown = function ($scope){
@@ -93,4 +93,84 @@ var Dropdown = function ($scope){
 };
 
 
-// our controller for the form
+app.controller('paymentController', function($scope) {
+  var json = {
+"widget":
+  {"http_response":[
+      { "is_ok": true,
+        "is_success": true,
+        "is_redirection": false,
+        "is_redirect": false,
+        "is_forbidden": false,
+        "is_not_found": false,
+        "is_client_error": false,
+        "is_server_error": false }
+  ]},
+  "data":[
+  { "payment_id": "1444",
+    "payment_method_id": "5",
+    "payment_method_info": null,
+    "payment_method_info2": "9",
+    "payment_method_info3": null,
+    "payment_status": "1",
+    "payment_method_key": "ONLINE",
+    "payment_method_value": "Veritrans"
+  },
+  { "payment_id": "1445",
+    "payment_method_id": "4",
+    "payment_method_info": "PT. Global Loket Sejahtera\r\nBANK : BCA (Branch Mall Pondok Indah)\r\nAccount : 731.033.7344 \r\n",
+    "payment_method_info2": null,
+    "payment_method_info3": null,
+    "payment_status": "0",
+    "payment_method_key": "OFFLINE",
+    "payment_method_value": "Bank Transfer"
+    },
+    { "payment_id": "1446",
+      "payment_method_id": "9",
+      "payment_method_info": "",
+      "payment_method_info2": null,
+      "payment_method_info3": null,
+      "payment_status": "0",
+      "payment_method_key": "ONLINE",
+      "payment_method_value": "VA Permata"
+     }
+    ],
+      "inquiry":{
+        "status_message":"success"
+      }
+};
+
+  $scope.ocw = json;
+
+  //----------------
+  //    Accordion
+  //----------------
+
+
+  $scope.oneAtATime = true;
+
+  $scope.items = ['Item 1', 'Item 2', 'Item 3'];
+
+  $scope.addItem = function() {
+    var newItemNo = $scope.items.length + 1;
+    $scope.items.push('Item ' + newItemNo);
+  };
+
+});
+
+var mapsController = function (){
+
+};
+
+
+app.controller('legendController', function ($scope, $modal, $log) {
+
+  $scope.open = function () {
+
+    var modalInstance = $modal.open({
+      templateUrl: 'views/partials/modal.html',
+      controller: 'legendController',
+    });
+
+  };
+});
